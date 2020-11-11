97e00edc1fbf637e9f0cbe31d2c8737fe80b31ec
Jace Ju <jaceju@gmail.com>
2020-11-11 11:54:52 +0800

    Add tmux config

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 mv ~/.zshrc ~/.zshrc.bak
 ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux-osx.conf ~/.tmux-osx.conf
 mv ~/.oh-my-zsh/themes/taybalt.zsh-theme ~/.oh-my-zsh/themes/taybalt.zsh-theme.bak
 ln -s ~/.dotfiles/zsh/themes/taybalt.zsh-theme ~/.oh-my-zsh/themes/taybalt.zsh-theme
